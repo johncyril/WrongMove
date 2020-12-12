c3f5d9d68e162e9c07fc28f7b00132eaa6990458
johncyril <johncyril0@gmail.com>
2020-12-12 18:50:41 +0000

    Turning into Hosted WebAssembly

--- WrongMove/Server/Program.cs
+++ WrongMove/Server/Program.cs
@@ -1,15 +1,13 @@
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WrongMove
+namespace WrongMove.Server
 {
     public class Program
     {
